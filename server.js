6f527fda63bfe9b2aab599c83348dd0b4b7ae8f5
peccu <peccul@gmail.com>
2019-02-14 10:19:43 +0900

    remove unused console.log

--- server.js
+++ server.js
@@ -47,7 +47,6 @@ function Server (opts) {
   this.watch = function (_path) {
     var self = this
     chokidar.watch(_path).on('change', function (_path, stats) {
-      console.log('change detected')
       self.emitContent(_path)
     })
   }
