4262be70fbc890fa15a9c7a54f8882da7bf944fc
peccu <peccul@gmail.com>
2019-02-14 21:49:23 +0900

    promisify renderer for supporting more renderer

--- renderer/index.js
+++ renderer/index.js
@@ -1,12 +1,19 @@
 const markdown = require('./markdown')
 const plantuml = require('./plantuml')
 
+const wrap = (render) => {
+  return (src, cb) => cb(null, render(src))
+}
+
+const id = src => src
+const pre = src => `<pre>${src}</pre>`
+
 const renderer = {
-  md: markdown,
-  plantuml: plantuml,
-  puml: plantuml,
-  svg: src => src,
-  _: src => `<pre>${src}</pre>`
+  md: wrap(markdown),
+  plantuml: wrap(plantuml),
+  puml: wrap(plantuml),
+  svg: wrap(id),
+  _: wrap(pre)
 }
 
 module.exports = renderer
